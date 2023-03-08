484ccfaf912e90abbbf4a558d4dfda5a794b5461
alexsander639 <alexsander639@gmail.com>
2023-03-08 11:56:13 -0300

    commit final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br_com_dio_desafio.dominio.Bootcamp;
 import br_com_dio_desafio.dominio.Curso;
+import br_com_dio_desafio.dominio.Dev;
 import br_com_dio_desafio.dominio.Mentoria;
 
 public class Main {
@@ -20,8 +22,36 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAlex = new Dev();
+        devAlex.setNome("Alex");
+        devAlex.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Alex:" + devAlex.getConteudosInscritos());
+        devAlex.progredir();
+        devAlex.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Alex:" + devAlex.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Alex:" + devAlex.getConteudosConcluidos());
+        System.out.println("XP:" + devAlex.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
     }
 }
--- src/br_com_dio_desafio/dominio/Curso.java
+++ src/br_com_dio_desafio/dominio/Curso.java
@@ -1,25 +1,11 @@
 package br_com_dio_desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
     private int cargaHoraria;
     
     public Curso() {
     }
-    
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -29,7 +15,12 @@ public class Curso {
     
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
     
 }
