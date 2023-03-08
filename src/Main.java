76afaf76d496fe48fff44828ff8ba14efbb0497b
alexsander639 <alexsander639@gmail.com>
2023-03-08 11:14:14 -0300

    mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+import br_com_dio_desafio.dominio.Curso;
+import br_com_dio_desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("menteria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
